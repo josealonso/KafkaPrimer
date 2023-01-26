7e2ec50e19b54fe3838ac19660550d256cc52d3a
josealonso <jose.alonso.programmer@gmail.com>
2023-01-26 23:20:38 +0100

    Configured and created the other Kafka consumer.

--- springboot-kafka-microservices/email-service/src/main/java/info/josealonso/kafkademo/emailservice/kafka/OrderConsumer.java
+++ springboot-kafka-microservices/email-service/src/main/java/info/josealonso/kafkademo/emailservice/kafka/OrderConsumer.java
@@ -1,11 +1,10 @@
-package info.josealonso.kafkademo.stockservice.kafka;
+package info.josealonso.kafkademo.emailservice.kafka;
 
 import info.josealonso.kafkademo.basedomains.dto.OrderEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-
 
 @Service
 public class OrderConsumer {
@@ -17,9 +16,8 @@ public class OrderConsumer {
             groupId = "${spring.kafka.consumer.group-id}"
     )
     public void consume(OrderEvent event) {
-        LOGGER.info(String.format("Order event received in stock service => %s", event.toString()));
+        LOGGER.info(String.format("Order event received in email service => %s", event.toString()));
 
-        // TODO: save in a database
+        // TODO: send an email to the customer
     }
 }
-
